b1ad4e3e2b5f248ab2204c19f1a80b7c7c725f30
Mike Rettig <mike.rettig@gmail.com>
2012-05-20 17:10:03 -0500

    init

--- test/conf/init_db.sql
+++ test/conf/init_db.sql
@@ -1,1 +1,3 @@
-create table datch_version(version text , file text, host text, user text, timestamp text);
+create table datch_version(version text not null , file text not null,
+  host text, user text, timestamp text,
+  primary key(version, file));
