2c838004eccaa6978431be50a1e97071b9899f6f
Mike Rettig <mike.rettig@gmail.com>
2012-05-19 23:49:48 -0500

    init

--- test/example/0001.1.first_patch.rb
+++ test/example/0001.1.first_patch.rb
@@ -1,6 +1,11 @@
 require File.dirname(__FILE__) +"/../../lib/datch/sql_patch.rb"
 
 def datch(context)
-  change="alter table one;"
+  change=<<eod
+alter table one;
+add index table one;
+insert into table one values;
+--other stuff;
+eod
   SqlPatch.new change
 end
