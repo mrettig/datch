8ee92abcd280ddb97e74387129d9ac6d769d8cac
Mike Rettig <mike.rettig@gmail.com>
2012-05-20 12:09:28 -0500

    init

--- lib/datch/datch_main.rb
+++ lib/datch/datch_main.rb
@@ -1,10 +1,29 @@
 require File.dirname(__FILE__) + "/datch.rb"
 
-dir=ARGV.shift
-output=ARGV.shift
-load ARGV.shift
-db=configure
+command = ARGV.shift
 
-parser = DatchParser.new(dir, db)
-parser.write_change_sql(output)
-parser.write_rollback_sql(output)
+commands={}
+commands['init_db'] = lambda{
+  load ARGV.shift
+  db=configure
+  db.init
+}
+
+commands['diff'] = lambda {
+  dir=ARGV.shift
+  output=ARGV.shift
+  load ARGV.shift
+  db=configure
+  parser = DatchParser.new(dir, db)
+  parser.write_change_sql(output)
+  parser.write_rollback_sql(output)
+}
+
+if commands.include? command
+  commands[command].call
+else
+  puts "Command #{command} not found in : #{commands.keys.inspect}"
+  exit -1
+end
+
+
