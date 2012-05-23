1e0831fb06f4eb5fa372cc975d846686a102149a
mrettig <mrettig@drw.com>
2012-05-23 12:57:53 -0500

    add oracle sqlplus example.

--- lib/datch/sqlite3_db.rb
+++ lib/datch/sqlite3_db.rb
@@ -8,11 +8,11 @@ module Datch
   class Sqlite3Db
 
     def initialize(db_file='datch.sqlite.db')
-      @db=db_file
+      @db=File.expand_path(db_file)
     end
 
     def init_db
-      exec_sql "create table datch_version(version integer not null , file_name text not null, host text, user_name text, timestamp text, primary key(version));"
+      exec_sql "create table datch_version(schema_name text not null, version integer not null , file_name text not null, host text, user_name text, change_timestamp text, primary key(schema_name, version));"
     end
 
     def exec_sql(sql)
@@ -49,11 +49,11 @@ eod
     end
 
     def create_version_update_sql(file)
-      "insert into datch_version (file_name,version, host, user_name, timestamp) values ('#{file.name}',#{file.version}, '#{Socket.gethostname}', '#{ENV['USER']}', '#{DateTime.now.to_s}');"
+      "insert into datch_version (schema_name, file_name,version, host, user_name, change_timestamp) values ('#@db', '#{file.name}',#{file.version}, '#{Socket.gethostname}', '#{ENV['USER']}', '#{DateTime.now.to_s}');"
     end
 
     def create_version_rollback_sql(file)
-      "delete from datch_version where version=#{file.version};"
+      "delete from datch_version where version=#{file.version} and schema_name='#@db';"
     end
 
   end
